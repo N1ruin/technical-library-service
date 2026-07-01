aeb8106a883f98e13302a00e3dd40b8d497edd87
Евгений <n1ruin123@gmail.com>
2026-07-01 23:42:50 +0300

    fix

--- src/main/java/by/niruin/library/mapper/JwtAuthenticatonConverter.java
+++ src/main/java/by/niruin/library/mapper/JwtAuthenticatonConverter.java
@@ -5,6 +5,7 @@ import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.stereotype.Component;
 
 import java.util.Collection;
@@ -15,7 +16,9 @@ import java.util.stream.Collectors;
 public class JwtAuthenticatonConverter implements Converter<Jwt, AbstractAuthenticationToken> {
     @Override
     public AbstractAuthenticationToken convert(Jwt jwt) {
-        return null;
+        var roles = extractRoles(jwt);
+
+        return new JwtAuthenticationToken(jwt, roles);
     }
 
     private Collection<GrantedAuthority> extractRoles(Jwt jwt) {
